ccff87e18eeebf5b2c1f0fb78d193bb9ad5a977d
Leonardo Camargo <leonardo_camargo31@hotmail.com>
2023-11-06 19:16:01 -0300

    chore: add sequelize and @nestjs/sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TweetsModule } from './tweets/tweets.module';
 
+// decorators
 @Module({
-  imports: [],
+  imports: [TweetsModule],
   controllers: [AppController],
   providers: [AppService],
 })
