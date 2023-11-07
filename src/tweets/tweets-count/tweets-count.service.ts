4a53106105e5fba351c82e3069dbed2c4bdff0ad
Leonardo Camargo <leonardo_camargo31@hotmail.com>
2023-11-07 10:27:59 -0300

    chore: add @nestjs/cache-manage and cache-manager

--- src/tweets/tweets-count/tweets-count.service.ts
+++ src/tweets/tweets-count/tweets-count.service.ts
@@ -1,21 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { Tweet } from '../entities/tweet.entity';
 import { InjectModel } from '@nestjs/sequelize';
 import { Interval } from '@nestjs/schedule';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class TweetsCountService {
+  private limit = 10;
   constructor(
     @InjectModel(Tweet)
     private tweetModel: typeof Tweet,
+    @Inject(CACHE_MANAGER)
+    private cacheManager: Cache,
   ) {}
 
   // de quanto em quanto tempo que vai executar
   @Interval(5000)
   async countTweets() {
+    let offset = (await this.cacheManager.get('tweet-offset')) as number;
+    offset = offset === undefined ? 0 : offset;
+
+    console.log(`offsets: ${offset}`);
+
     const tweets = await this.tweetModel.findAll({
-      offset: 0,
-      limit: 10,
+      offset,
+      limit: this.limit,
     });
+
+    console.log(`${tweets.length} encontrados`);
+
+    if (tweets.length === this.limit) {
+      this.cacheManager.set('tweet-offset', offset + this.limit, 1 * 60 * 10);
+
+      console.log(`achou ${this.limit} tweets`);
+    }
   }
 }
